f84013b3e0e5b3c754725ff48d4a5e95c235bc9e
zfeng <zi7feng7@gmail.com>
2024-07-05 14:13:09 -0400

    remove clone

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 fn main() {
     println!("Hello, world!");
     let a = String::from("10");
-    let b = a.clone(); // Use clone() to make a copy instead of moving ownership
+    let b = a; // Use clone() to make a copy instead of moving ownership
     println!("{:?}", b);
     println!("{:?}", b);
-    // println!("{}", a); // Uncommenting this line will cause a compile error due to ownership transfer
-
     let c = test_fun();
     println!("{} ", c);
 }
