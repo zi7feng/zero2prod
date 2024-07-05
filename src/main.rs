21bb9e885c2468aa407ba8ca785ea1a2617b9091
zfeng <zi7feng7@gmail.com>
2024-07-05 13:56:00 -0400

    gpt version

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
     println!("Hello, world!");
     let a = String::from("10");
-    let b = a;
+    let b = a.clone(); // Use clone() to make a copy instead of moving ownership
     println!("{:?}", b);
     println!("{:?}", b);
-    // println!("{}", a);
+    // println!("{}", a); // Uncommenting this line will cause a compile error due to ownership transfer
+
     let c = test_fun();
     println!("{} ", c);
 }
@@ -13,11 +14,12 @@ fn test_fun() -> i32 {
     4
 }
 
-// fn test2() {
-//     println!("fdstat");
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn test_main() {
-    assert_eq!(1, 1);
+    #[test]
+    fn test_main() {
+        assert_eq!(1, 1);
+    }
 }
