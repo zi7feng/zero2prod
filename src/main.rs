c6359e57b2b964777ba31b2a31baa1b254e3a07e
zfeng <zi7feng7@gmail.com>
2024-07-05 13:26:08 -0400

    test 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
+    let a = String::from("10");
+    let b = a;
+    println!("{:?}", b);
+    println!("{:?}", b);
+    // println!("{}", a);
+    let c = test_fun();
+    println!("{} ", c);
+}
+
+fn test_fun() -> i32 {
+    4
+}
+
+fn test2() {
+    println!("fdstat");
+}
+
+#[test]
+fn test_main() {
+    assert_eq!(1, 1);
 }
